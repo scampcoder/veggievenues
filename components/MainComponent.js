92052ee024f4d7aafc619b0d588cef2e9cae1aaf
Victoria Kee <victoriakee95@gmail.com>
2022-01-04 16:56:05 -0600

    initialize state for main

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,3 +2,15 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasLocationPermission: false,
+            latitude: 0,
+            longitude: 0,
+            restaurantList: []
+        }
+    }
+}
