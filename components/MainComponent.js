b0552eeab97f9e71d2ff7fb20f6aaeb6b3dadc55
Victoria Kee <victoriakee95@gmail.com>
2022-01-04 16:54:28 -0600

    set up main component with imports

---
+++ components/MainComponent.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
