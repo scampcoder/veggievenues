a99437dfcaedb9319b5f71bd49476085c411cd1d
Victoria Kee <victoriakee95@gmail.com>
2022-01-04 20:44:35 -0600

    fix typos

--- App.js
+++ App.js
@@ -10,60 +10,61 @@ export default class App extends Component {
       restaurantList: []
   }
 
-componentDidMount() {
-  this.getLocationAsync();
-}
-
-async getLocationAsync() {
-  let location = await Location.getCurrentPositionAsync({});
-  this.setState({
-      hasLocationPermission: true,
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-  });
-};
-
-handleRestaurantSearch = () => {
-  //baseURL
-  const url  = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-  //add user's location to state lat & long
-  const location = `location=${this.state.latitude},${this.state.longitude}`;
-  //radius of search in meters
-  const radius = '&radius=200';
-  //type of search
-  const type = '&keyword=restaurant';
-  //our API key
-  const key = '&key=AIzaSyBWWJUImGj4nj1W4N3g34yvwmbVZMJ_bBg';
-  //add everything together so it can be passed as the API call
-  const restaurauntSearchURL = url + location + radius + type + key;
-  //fetch request for the API call
-  fetch(restaurauntSearchURL)
-    //handle when the promise is resolved and parse the JSON we were sent from Google
-    .then(response => response.json())
-    //change the restaurantList key within our state to match whatever we were sent back by this fetch request
-    .then(result => this.setState({restaurantList: result}))
-}
-
-
-render() {
-  //testing
-  console.log(this.state.restaurantList);
-    return (
-        <View style={styles.container}>
-          <FlatList
-            data={this.state.restaurantList.results}
-            keyExtractor={(item) => item.place_id}
-            renderItem={({item}) => {
-              <Text>{item.name}</Text>
-            }}
-            style={styles.listName}
-          />
-          <Pressable onPress={() => this.handleRestaurantSearch()}>
-              <Text style={styles.search}>Find Veggies</Text>
-          </Pressable>
-        </View>
-    )
+  //upon app mounting, the location grabbing method will run
+  componentDidMount() {
+    this.getLocationAsync();
   }
+
+  async getLocationAsync() {
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+        hasLocationPermission: true,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+    });
+  };
+
+  handleRestaurantSearch = () => {
+    //baseURL
+    const url  = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+    //add user's location to state lat & long
+    const location = `location=${this.state.latitude},${this.state.longitude}`;
+    //radius of search in meters
+    const radius = '&radius=200';
+    //type of search
+    const type = '&keyword=restaurant';
+    //our API key
+    const key = '&key=AIzaSyBWWJUImGj4nj1W4N3g34yvwmbVZMJ_bBg';
+    //add everything together so it can be passed as the API call
+    const restaurantSearchURL = url + location + radius + type + key;
+    //fetch request for the API call
+    fetch(restaurantSearchURL)
+      //handle when the promise is resolved and parse the JSON we were sent from Google
+      .then(response => response.json())
+      //change the restaurantList key within our state to match whatever we were sent back by this fetch request
+      .then(result => this.setState({restaurantList: result}))
+  }
+
+
+  render() {
+    //testing
+    console.log(this.state.restaurantList);
+      return (
+          <View style={styles.container}>
+            <FlatList
+              data={this.state.restaurantList.results}
+              keyExtractor={(item) => item.place_id}
+              renderItem={({item}) => {
+                <Text>{item.name}</Text>
+              }}
+              style={styles.listName}
+            />
+            <Pressable onPress={() => this.handleRestaurantSearch()}>
+                <Text style={styles.search}>Find Veggies</Text>
+            </Pressable>
+          </View>
+      )
+    }
 }
 
 const styles = StyleSheet.create({
