92212945b59f6c21233f23411cbf204a7dec2b1a
Victoria Kee <victoriakee95@gmail.com>
2022-01-04 19:35:24 -0600

    issues w/ component styling, moved all to app

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './components/MainComponent';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View>
-      <Main />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        hasLocationPermission: false,
+        latitude: 0,
+        longitude: 0,
+        restaurantList: []
+    }
+}
+render() {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity >
+                <Text style={styles.search}>Find Veggies</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
 }
 
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+},
+search: {
+    backgroundColor: '#F23CA1',
+    textAlign: 'center',
+    padding: 20, 
+    marginTop: 50,
+    fontSize: 20,
+    color: 'white'
+}
+})
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
 class Main extends Component {
     constructor(props) {
@@ -15,13 +16,37 @@ class Main extends Component {
     }
     render() {
         return (
-            <View>
-                <TouchableOpacity>
-                    <Text>Find Veggies</Text>
-                </TouchableOpacity>
+            <View style={styles.view}>
+                <View style={styles.container}>
+                    <TouchableOpacity >
+                        <Text style={styles.search}>Find Veggies</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1, 
+        paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+    search: {
+        backgroundColor: '#F23CA1',
+        textAlign: 'center',
+        padding: 20, 
+        marginTop: 50,
+        fontSize: 20,
+        color: 'white'
+    }
+})
 
 export default Main;
